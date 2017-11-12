62628315280dbb9c959030549f465df0a603543e
Jason Czubkowski <Jason.Czubkowski@generac.com>
2017-11-12 10:08:34 -0600

    Showing branch functionality

--- src/main/java/ChocAnServiceDriver.java
+++ src/main/java/ChocAnServiceDriver.java
@@ -2,5 +2,6 @@ public class ChocAnServiceDriver {
 
     public static void main(String[] args){
         System.out.println("Hello from local repository.");
+        System.out.println("Demo of a feature branch.");
     }
 }
