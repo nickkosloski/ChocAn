bcb85525fa068811b771264fab1ca2088bf8be24
nickkosloski <nkoslosk@pio.carrollu.edu>
2017-11-12 10:13:21 -0600

    added two lines

--- src/main/java/ChocAnServiceDriver.java
+++ src/main/java/ChocAnServiceDriver.java
@@ -2,5 +2,7 @@ public class ChocAnServiceDriver {
 
     public static void main(String[] args){
         System.out.println("Hello from local repository.");
+
+        System.out.println("GIT ER DONE");
     }
 }
