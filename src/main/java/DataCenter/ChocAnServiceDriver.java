fcf08007dac6f66980e0300f3de74da4548306ab
SamiNiki8 <snicoleh20@gmail.com>
2017-11-19 10:57:39 -0600

    Properties files and Organization

--- src/main/java/DataCenter/ChocAnServiceDriver.java
+++ src/main/java/DataCenter/ChocAnServiceDriver.java
@@ -7,11 +7,6 @@ public class ChocAnServiceDriver {
     public static void main(String[] args)
     {
 
-        //DataCenter.DataCenter chocAnCenter = new DataCenter.DataCenter();
-
-        Resources res = new Resources();
-        System.out.println(res.getUsername());
-        System.out.println(res.getPassword());
-        System.out.println(res.getConnection());
+        DataCenter chocAnCenter = new DataCenter();
     }
 }
