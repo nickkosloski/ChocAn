7cc06400ed09493e1f9e7798fa079398cbb07e81
GENERAC\JCzubkow <jason.czubkowski@generac,com>
2017-12-19 21:36:23 -0600

    Scheduled report tasks.

--- src/main/java/DataCenter/ChocAnServiceDriver.java
+++ src/main/java/DataCenter/ChocAnServiceDriver.java
@@ -1,6 +1,10 @@
 package DataCenter;
 
 import Frames.DataCenterFrame;
+import Utils.ReportGenerator;
+
+import java.util.Calendar;
+import java.util.Timer;
 
 public class ChocAnServiceDriver {
 
@@ -8,5 +12,25 @@ public class ChocAnServiceDriver {
     {
         DataCenterFrame chocAnCenter = new DataCenterFrame();
         chocAnCenter.createAndShowGUI();
+        scheduleReports();
+    }
+
+    private static void scheduleReports(){
+        Timer timer = new Timer();
+        Calendar date = Calendar.getInstance();
+        date.set(
+                Calendar.DAY_OF_WEEK,
+                Calendar.FRIDAY
+        );
+        date.set(Calendar.HOUR, 21);
+        date.set(Calendar.MINUTE, 0);
+        date.set(Calendar.SECOND, 0);
+        date.set(Calendar.MILLISECOND, 0);
+        // Schedule to run every Sunday in midnight
+        timer.schedule(
+                new ReportGenerator(),
+                date.getTime(),
+                1000 * 60 * 60 * 24 * 7
+        );
     }
 }
